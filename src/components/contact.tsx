c2c515336fd451a6367558b755d801512739b103
guitarisprettierthanpiano <shfabrizio@gmail.com>
2021-12-29 05:21:22 -0500

    added parallax

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -5,11 +5,19 @@ const Contact: React.FC = () => {
     return (
     <div className='contact-container'>
 
-        <div className='contact-landing'>
-            
+        <div className="parallax">        </div>
+
+        <div className='testt'>
+        Scroll Up and Down this page to see the parallax scrolling effect.
+        This div is just here to enable scrolling.
+        Tip: Try to remove the background-attachment property to remove the scrolling effect.
+
         </div>
 
+        <div className='testy'>NEXT STUFF</div>
+
     </div>
+
     )
 }
 
