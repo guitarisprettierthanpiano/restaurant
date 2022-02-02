6a571b1bbe9fa9f2eba227f88aaff3b81a5b8231
guitarisprettierthanpiano <shfabrizio@gmail.com>
2022-02-02 05:55:35 -0500

    updated events for feb 2022

--- src/components/events.tsx
+++ src/components/events.tsx
@@ -12,34 +12,32 @@ const Events: React.FC = () => {
                 <h5>Music Times</h5>
                 <p>Fri & Sat 7pm to 11pm, Sunday 3pm to 7pm.  Local musicians every other Tuesday 5pm to 9pm. (Unless otherwise specified below).</p>
 
-                <h4>January</h4>
+                <h4>February</h4>
                 <div className='left-events'>
-                        <p>1 - Pink Street Duo - 7pm - 11pm</p>
-                        <p>2 - Corey Ave. Sunday Market / Bucs. vs. Jets 1pm</p>
-                        <p>3 - Trivia 7pm & Monday Night Football</p>
-                        <p>4 - Kevin Toon 5pm - 9pm</p>
-                        <p>6 - Game Show Night 7pm</p>
-                        <p>7 - The Torpedoes 7pm - 11pm</p>
-                        <p>8 - Corey Ave. Craft Show / John Kyle Rhode 7pm - 11pm</p>
-                        <p>9 - Corey Ave. Craft Show / Bucs. vs. Panthers 1pm</p>
-                        <p>10 - Trivia 7pm</p>
-                        <p>11 - Paint Night 6:30pm</p>
-                        <p>14 - tba</p>
+                        <p>1 - Game Show Night 7pm</p>
+                        <p>2 - Trivia 7pm</p>
+                        <p>4 - Jason Alan 7pm - 11pm</p>
+                        <p>5 - tba</p>
+                        <p>6 - Corey Ave. Market / One Kin Roof Fundraiser 2pm - 6pm</p>
+                        <p>7 - Trivia 7pm</p>
+                        <p>8 - Kevin Toon 5pm - pm</p>
+                        <p>9 - Paint Night 6:30pm</p>
+                        <p>11 - HighLife Band 7pm - 11pm</p>
+                        <p>12 - Pete & Dean 3pm - 7pm</p>
+                        <p>13 - Super Bowl 6pm</p>
                 </div>
                 <div className='right-events'>
-                        <p>15 - Pete & Dean</p>
-                        <p>16 - Corey Ave. Sunday Market / Football</p>
-                        <p>17 - Trivia **6pm start</p>
-                        <p>21 - Charles Parker 7pm - 11pm</p>
-                        <p>22 - Football - Wildcard</p>
-                        <p>23 - Football - Wildcard</p>
-                        <p>24 - Trivia 7pm</p>
-                        <p>25 - Kevin Toon 5pm - 9pm</p>
-                        <p>26 - Paint Night 6:30pm</p>
-                        <p>28 - Jason Alan 7pm - 11pm</p>
-                        <p>29 - Doug South 7pm - 11pm</p>
-                        <p>30 - tba</p>
-                        <p>31 - Trivia 7pm</p>
+                        <p>14 - Trivia 7pm</p>
+                        <p>18 - tba</p>
+                        <p>19 - HorsesWild Band 3pm - 7pm - Guest Bartenders!</p>
+                        <p>20 - tba</p>
+                        <p>21 - Trivia 7pm</p>
+                        <p>22 - Kevin Toon 5pm - 9pm</p>
+                        <p>23 - Game Show Night 7pm</p>
+                        <p>24 - Paint Nite 6:30pm</p>
+                        <p>25 - tba</p>
+                        <p>26 - tba</p>
+                        <p>28 - Trivia 7pm</p>
                 </div> 
 
                 <h3>Happy Hour 7 Days A Week!</h3>
